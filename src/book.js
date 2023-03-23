36313114e8679c4cf808b6ca4c1d402b75781d51
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-23 14:24:38 -0700

    Pass tests for createTitle and buildMainCharacter

--- src/book.js
+++ src/book.js
@@ -1,11 +1,25 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+};
+
+function saveReview(review, reviews) {
+  reviews.push(review);
+};
+
+// console.log(saveReview("An astounding success", reviews));
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
