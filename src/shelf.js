8bd222f760f49a1424348dac2ed3c1fafc033466
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-24 08:38:57 -0700

    Pass tests for shelfBook function

--- src/shelf.js
+++ src/shelf.js
@@ -1,9 +1,24 @@
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
 
-function shelfBook
+function shelfBook(book, shelf) {
+  // access genre
+  // write conditional to keep length of shelf under <= 3
+  if (shelf.length === 3) {
+    return null;
+    } 
+  return shelf.unshift(book);
+};
+
+function unshelfBook() {
+
+}
+
+
+
+
