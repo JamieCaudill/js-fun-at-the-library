53463ce2d7d612ade6c56564d7c95ffa4318b22a
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-24 13:00:30 -0700

    Pass tests for checkoutBook function

--- src/library.js
+++ src/library.js
@@ -28,38 +28,14 @@ function addBook(library, book) {
 function checkoutBook(library, book, genre) {
   // access book on shelf based on genre
   var shelf = library.shelves[genre];
-  shelf.pop();
-  return (`You have now checked out ${book} from the ${library.name}.`)
+  // for loop to iterate through the books on shelf.
+  for (var i = 0; i < shelf.length; i++) {
+    // conditional to check if book === book on shelf.
+    if (book === shelf[i].title) {
+       shelf.splice(i, 1); 
+       return (`You have now checked out ${book} from the ${library.name}.`)
+    } 
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}.` 
 };
 
-var dracula = {
-  title: "Dracula",
-  mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
-  pageCount: 418,
-  genre: "fantasy"
-};
-
-var bornACrime = {
-  title: "Born a Crime",
-  mainCharacter: { name: "Trevor Noah", age: 36, pronouns: "he/him" },
-  pageCount: 304,
-  genre: "nonFiction"
-};
-
-var prideAndPrejudice = {
-  title: "Pride and Prejudice",
-  mainCharacter: { name: "Eliabeth Bennet", age: 20, pronouns: "she/her" },
-  pageCount: 432,
-  genre: "fiction"
-};
-
-var denverLibrary = createLibrary("Denver Public Library");
-
-addBook(denverLibrary, dracula);
-addBook(denverLibrary, bornACrime);
-addBook(denverLibrary, prideAndPrejudice);
-
-console.log(denverLibrary.shelves);
-var result1 = checkoutBook(denverLibrary, "Pride and Prejudice", "fiction");
-
-console.log(denverLibrary.shelves);
